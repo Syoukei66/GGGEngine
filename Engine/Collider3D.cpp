d9428edcc4a1401ab1c67542b4ab65d68671bff0
Uehara <uekarakuruzo527@yahoo.co.jp>
2018-01-06 10:13:23 +0900

    Sphare_Sphare

--- Engine/Collider3D.cpp
+++ Engine/Collider3D.cpp
@@ -1,16 +1,55 @@
 #include "Collider3D.h"
+#include "Collision3D.h"
 
+// =================================================================
+// Methods
+// =================================================================
+Collider3D::Collider3D(GameObject3D* entity)
+  : entity_(entity)
+{
+}
+
+void Collider3D::Prepare()
+{
+  this->position_ = this->entity_->GetTransform()->GetWorldPosition();
+}
+
+bool Collider3D::RoughCheck(const Collider3D& other)
+{
+  return Collision::Sphare_Sphare(
+    this->GetPosition(),
+    this->GetBoundRadius(),
+    other.GetPosition(),
+    other.GetBoundRadius()
+    );
+}
 
 // =================================================================
 // Sphare
 // =================================================================
-//Collider3D_Sphare::Collider3D_Sphare()
+Collider3D_Sphare::Collider3D_Sphare(GameObject3D* entity)
+  : Collider3D(entity)
+{
+}
+
+bool Collider3D_Sphare::Collision(Collider3D_Sphare& other)
+{
+  this->Prepare();
+  other.Prepare();
+  Collision::Sphare_Sphare(
+    this->GetPosition(),
+    this->GetBoundRadius(),
+    other.GetPosition(),
+    other.GetBoundRadius()
+  );
+}
+
+//bool Collider3D_Sphare::Collision(const Collider3D_Capsule & other)
 //{
+//  return false;
 //}
-//
-//// =================================================================
-//// Capsule
-//// =================================================================
-//Collider3D_Capsule::Collider3D_Capsule()
-//{
-//}
+
+T_FLOAT Collider3D_Sphare::GetBoundRadius() const
+{
+  return this->radius_;
+}
--- Engine/Collider3D.h
+++ Engine/Collider3D.h
@@ -2,23 +2,44 @@
 
 #include "GameComponent.h"
 #include "Geometry.h"
+#include "GameObject3D.h"
 
 class Collider3D_Sphare;
-class Collider3D_Capsule;
+//class Collider3D_Capsule;
 
 class Collider3D : public GameComponent
 {
+  // =================================================================
+  // Constructor / Destructor
+  // =================================================================
 public:
-  virtual TVec3f& BoundPosition() = 0;
-  virtual T_FLOAT BoundRadius() = 0;
+  Collider3D(GameObject3D* entity);
+
+  // =================================================================
+  // Methods
+  // =================================================================
+public:
+  void Prepare();
+  virtual bool Collision(Collider3D_Sphare& other) = 0;
+  //virtual bool Collision(const Collider3D_Capsule& other) = 0;
+
+protected:
   bool RoughCheck(const Collider3D& other);
 
+  // =================================================================
+  // Setter / Getter
+  // =================================================================
 public:
-  virtual bool Collision(const Collider3D_Sphare& other) = 0;
-  virtual bool Collision(const Collider3D_Capsule& other) = 0;
+  inline const TVec3f& GetPosition() const
+  {
+    return this->position_;
+  }
+  virtual T_FLOAT GetBoundRadius() const = 0;
 
-public:
-  const T_UINT8 kind;
+protected:
+  GameObject3D* entity_;
+  TVec3f position_;
+
 };
 
 // =================================================================
@@ -30,30 +51,53 @@ class Collider3D_Sphare : public Collider3D
   // Constructor / Destructor
   // =================================================================
 public:
-  Collider3D_Sphare();
+  Collider3D_Sphare(GameObject3D* entity);
 
   // =================================================================
-  // Setter / Getter
-  // =================================================================
-private:
-
-};
-
-// =================================================================
-// Capsule
-// =================================================================
-class Collider3D_Capsule : public Collider3D
-{
-  // =================================================================
-  // Constructor / Destructor
+  // Methods
   // =================================================================
 public:
-  Collider3D_Capsule();
+  virtual bool Collision(Collider3D_Sphare& other) override;
+  //virtual bool Collision(const Collider3D_Capsule & other) override;
+
+protected:
+  virtual T_FLOAT GetBoundRadius() const override;
 
   // =================================================================
   // Setter / Getter
   // =================================================================
-private:
+public:
+  inline T_FLOAT GetRadius() const
+  {
+    return this->radius_;
+  }
+  inline void SetRadius(T_FLOAT radius)
+  {
+    this->radius_ = radius;
+  }
 
+  // =================================================================
+  // Data Member
+  // =================================================================
+private:
+  T_FLOAT radius_;
 };
 
+//// =================================================================
+//// Capsule
+//// =================================================================
+//class Collider3D_Capsule : public Collider3D
+//{
+//  // =================================================================
+//  // Constructor / Destructor
+//  // =================================================================
+//public:
+//  Collider3D_Capsule(GameObject3D* entity);
+//
+//  // =================================================================
+//  // Setter / Getter
+//  // =================================================================
+//private:
+//
+//};
+//
