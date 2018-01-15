5e641ff59ec244ee4baec887a2c12466852cf7f9
SyoukeiMoronoki <uekarakuruzo527@yahoo.co.jp>
2018-01-15 09:28:34 +0900

    Transform Fix

--- Engine/Transform.cpp
+++ Engine/Transform.cpp
@@ -11,6 +11,7 @@ Transform::Transform(GameObject* entity)
   this->scale_matrix_ = INativeMatrix::Create();
   this->rotation_matrix_ = INativeMatrix::Create();
   this->matrix_ = INativeMatrix::Create();
+  this->world_matrix_ = INativeMatrix::Create();
 }
 
 Transform::~Transform()
@@ -19,6 +20,7 @@ Transform::~Transform()
   delete this->scale_matrix_;
   delete this->rotation_matrix_;
   delete this->matrix_;
+  delete this->world_matrix_;
 }
 
 // =================================================================
@@ -53,17 +55,16 @@ void Transform::OnRotationChanged()
 
 void Transform::OnWorldTransformDirty()
 {
-  this->world_position_dirty_ = true;
+  this->world_transform_dirty_ = true;
 }
 
-bool Transform::UpdateMatrix()
+void Transform::UpdateMatrix()
 {
   bool matrix_dirty = false;
   if (this->translation_dirty_)
   {
     this->translate_matrix_->Init();
     this->UpdateTranslateMatrix(this->translate_matrix_);
-
     this->translation_dirty_ = false;
     matrix_dirty = true;
   }
@@ -81,12 +82,27 @@ bool Transform::UpdateMatrix()
     this->rotation_dirty_ = false;
     matrix_dirty = true;
   }
+  if (matrix_dirty)
+  {
+    this->matrix_->Init();
+    this->matrix_->Multiple(*this->scale_matrix_);
+    this->matrix_->Multiple(*this->rotation_matrix_);
+    this->matrix_->Multiple(*this->translate_matrix_);
+  }
+}
 
-  this->matrix_->Init();
-  this->matrix_->Multiple(*this->scale_matrix_);
-  this->matrix_->Multiple(*this->rotation_matrix_);
-  this->matrix_->Multiple(*this->translate_matrix_);
-
-  this->OnUpdateMatrix(this->matrix_);
-  return true;
+void Transform::UpdateWorldMatrix()
+{
+  if (this->world_transform_dirty_)
+  {
+    return;
+  }
+  this->world_matrix_->Init();
+  INativeMatrix* parent_world_matrix = this->GetParentWorldMatrix();
+  if (parent_world_matrix)
+  {
+    this->world_matrix_->Multiple(*parent_world_matrix);
+  }
+  this->world_matrix_->Multiple(*this->GetMatrix());
+  this->world_transform_dirty_ = false;
 }
